b409fb255b0f1473c86b5b3771d256bbbf3a8598
WhyM <WhyM>
2024-03-28 20:00:54 +0900

    프로젝트 설명 추가

--- src/constants/project.ts
+++ src/constants/project.ts
@@ -1,19 +1,19 @@
 import { theFirstEduImg, portfolioImg, nutritionImg, stackOverflowImg, goldenCityImg, ioImg, drLucidImg, gemmyImg } from "../assets/images/projectImage";
 
 const web = [
-  {name: "더퍼스트에듀", image: theFirstEduImg, detail: "교육을 통해 더 나은 세상을 만드는 기업 '더퍼스트에듀'의 홈페이지 제작을 요청받아 솔로 프로젝트로 제작하였습니다.", skill: ["React", "AWS"], linkUrl: "https://thefirstedu.net"},
-  {name: "(구)포트폴리오", image: portfolioImg, detail: "", skill: ["React", "AWS"], linkUrl: "https://poiuy4004.github.io/portfolio/"},
-  {name: "뉴트리션코더스", image: nutritionImg, detail: "", skill: ["React", "AWS"], linkUrl: "http://nutrients-coders.com/"},
-  {name: "스택오버플로우", image: stackOverflowImg, detail: "", skill: ["React", "AWS"], linkUrl: "http://se-sof.s3-website.ap-northeast-2.amazonaws.com/"},
-  {name: "경주!어디가?", image: goldenCityImg, detail: "", skill: ["React", "AWS"], linkUrl: "https://poiuy4004.github.io/goldencity/"},
-  {name: "포트폴리오 초안", image: ioImg, detail: "", skill: ["React", "AWS"], linkUrl: "https://poiuy4004.github.io/"},
+  {name: "더퍼스트에듀", image: theFirstEduImg, detail: "교육을 통해 더 나은 세상을 만드는 기업 '더퍼스트에듀'의 홈페이지 제작을 요청받아 솔로 프로젝트로 제작하였습니다.", skill: ["React", "AWS", "Styled-Components", "Zustand"], linkUrl: "https://thefirstedu.net"},
+  {name: "(구)포트폴리오", image: portfolioImg, detail: "이전 버전의 포트폴리오 입니다.", skill: ["React", "Github-Acttions", "Styled-Components", "Zustand"], linkUrl: "https://poiuy4004.github.io/portfolio/"},
+  {name: "뉴트리션코더스", image: nutritionImg, detail: "부트캠프에서 진행한 2차 팀 프로젝트로, 나만을 위한 맞춤 AI 식단 프로그램을 만들었습니다.", skill: ["React", "AWS", "Styled-Components", "Zustand"], linkUrl: "http://nutrients-coders.com/"},
+  {name: "스택오버플로우", image: stackOverflowImg, detail: "부트캠프에서 진행한 1차 팀 프로젝트로, 스택오버플로우를 카피하였습니다.", skill: ["React", "AWS", "Styled-Components", "Redux"], linkUrl: "http://se-sof.s3-website.ap-northeast-2.amazonaws.com/"},
+  {name: "경주!어디가?", image: goldenCityImg, detail: "가족여행 겸 공공데이터API를 테스트하기 위한 경주 여행정보 요약 사이트를 만들었습니다.", skill: ["공공데이터API", "React", "Github-Acttions", "Styled-Components"], linkUrl: "https://poiuy4004.github.io/goldencity/"},
+  {name: "포트폴리오 초안", image: ioImg, detail: "가장 처음 만들었던 포트폴리오 입니다.", skill: ["React", "Github-Acttions", "Styled-Components", "Redux"], linkUrl: "https://poiuy4004.github.io/"},
 ]
 const app = [
-  {name: "닥터루시드", image: drLucidImg, detail: "", skill: ["React", "AWS"], linkUrl: ""},
-  {name: "재미스튜디오", image: gemmyImg, detail: "", skill: ["React", "AWS"], linkUrl: ""},
+  {name: "닥터루시드", image: drLucidImg, detail: "바쁜 현대인들에겐 쉽고 빠른 진료를 제공하며, 의료진과 상담사들에게는 짜투리 경제활동을 제공하는 Win-Win App 닥터루시드를 개발중입니다.", skill: ["React-Native", "AWS", "Styled-Components", "Tab"], linkUrl: ""},
+  {name: "재미스튜디오", image: gemmyImg, detail: "개발자의 블로그, 개발자를 위한 커뮤니티, 개발자들의 공간 재미스튜디오를 개발중입니다.", skill: ["React", "AWS", "Styled-Components", "Redux"], linkUrl: ""},
 ]
 const ing = [
-  {name: "재미스튜디오", image: gemmyImg, detail: "", skill: ["React", "AWS"], linkUrl: ""},
-  {name: "닥터루시드", image: drLucidImg, detail: "", skill: ["React", "AWS"], linkUrl: ""},
+  {name: "재미스튜디오", image: gemmyImg, detail: "개발자의 블로그, 개발자를 위한 커뮤니티, 개발자들의 공간 재미스튜디오를 개발중입니다.", skill: ["React", "AWS", "Styled-Components", "Redux"], linkUrl: ""},
+  {name: "닥터루시드", image: drLucidImg, detail: "바쁜 현대인들에겐 쉽고 빠른 진료를 제공하며, 의료진과 상담사들에게는 짜투리 경제활동을 제공하는 Win-Win App 닥터루시드를 개발중입니다.", skill: ["React-Native", "AWS", "Styled-Components", "Tab"], linkUrl: ""},
 ]
   export { web, app, ing };
